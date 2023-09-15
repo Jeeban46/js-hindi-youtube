71677dcac63bb786d4ae68e3c02a0ca144df1116
Jeeban Ranjan Das <114843363+Jeeban46@users.noreply.github.com>
2023-09-15 18:25:23 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2 > 1); 
+console.log(2 >= 1);
+
+
+
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
