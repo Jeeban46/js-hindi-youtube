46c68945bd98b0f24f442fe880466228cc161963
Jeeban Ranjan Das <114843363+Jeeban46@users.noreply.github.com>
2023-09-23 13:55:45 +0000

    Objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,40 @@
+// singleton
+// object literals
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Jeeban",
+    "full name": "Jeeban Ranjan Das",
+    [mySym] : "myKey1",
+    age: 20,
+    location: "Jaipur",
+    email: "jeeban@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser.mySym)
+// console.log(typeof JsUser.mySym)
+// console.log(JsUser[mySym])
+
+JsUser.email = "jeeban@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "jeeban@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo())
